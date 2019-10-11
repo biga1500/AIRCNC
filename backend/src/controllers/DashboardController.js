2a19da74a48350787e957b368d62a257db95c3a3
Gabriel Patatt <gabriel.patatt@conectt.com.br>
2019-10-11 10:59:54 -0300

    back-end adjust routes

--- backend/src/controllers/DashboardController.js
+++ backend/src/controllers/DashboardController.js
@@ -3,7 +3,6 @@ const Spot = require('../models/Spot')
 module.exports = {
     async show(req, res) {
 
-        console.log( req.headers)
         const {user_id} = req.headers;
 
         const spots = await Spot.find({user: user_id});
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
+const cors = require ('cors');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
+const path = require('path');
 const app = express();
 
 mongoose.connect('mongodb+srv://gpatatt:gpatatt@cluster0-lyknz.mongodb.net/aircnc?retryWrites=true&w=majority', {
@@ -13,6 +15,8 @@ mongoose.connect('mongodb+srv://gpatatt:gpatatt@cluster0-lyknz.mongodb.net/aircn
 //req.query = acessar query params (filtros)
 //req.params = acessar route params(edição, delete)
 
+app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')))
 app.use(routes);
 app.listen(3333);
